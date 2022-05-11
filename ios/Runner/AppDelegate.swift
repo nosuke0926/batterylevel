c9c917dd084d90a3e097b500958854ec60300c0d
nagase ryunosuke <nagase0926@gmail.com>
2022-05-11 11:58:07 +0900

    ios側から充電残量を取得してFlutterで表示

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,60 @@
 import UIKit
 import Flutter
 
+enum ChannelName {
+    static let battery = "samples.flutter.io/battery"
+    static let charging = "samples.flutter.io/charging"
+}
+
+enum BatteryState {
+    static let charging = "charging"
+    static let discharging = "discharging"
+}
+
+enum MyFlutterErrorCode {
+    static let unavailable = "UNAVAILABLE"
+}
+
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        GeneratedPluginRegistrant.register(with: self)
+        
+        guard let controller = window?.rootViewController as? FlutterViewController else {
+            fatalError("rootViewController is not type FlutterViewController")
+        }
+        
+        let batteryChannel = FlutterMethodChannel(name: ChannelName.battery,
+                                                  binaryMessenger: controller.binaryMessenger)
+        batteryChannel.setMethodCallHandler({
+            (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+            // Note: this method is invoked on the UI thread.
+            // Handle battery messages.
+            guard call.method == "getBatteryLevel" else {
+                result(FlutterMethodNotImplemented)
+                return
+            }
+            self.receiveBatteryLevel(result: result)
+        })
+        
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+    
+    private func receiveBatteryLevel(result: FlutterResult) {
+        let device = UIDevice.current
+        device.isBatteryMonitoringEnabled = true
+        if device.batteryState == UIDevice.BatteryState.unknown {
+            result(FlutterError(code: MyFlutterErrorCode.unavailable,
+                                message: "Battery info unavailable",
+                                details: nil))
+        } else {
+            result(Int(device.batteryLevel * 100))
+        }
+    }
+
 }
+
+
